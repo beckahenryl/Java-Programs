4ac6fa26defcd9a1055e17fa0db2264c62f48303
Becka Henry <36617288+beckahenryl@users.noreply.github.com>
2018-11-10 18:53:37 -0800

    Add files via upload

---
+++ MessageWrite.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class MessageWrite
+{
+ public static void main(String[] args)
+ {
+   int i = 0, number_stars = 10;
+   
+   System.out.print("Stars: ");
+   while (i <=10){
+      System.out.print("*");
+      i++;
+   
+   }
+ 
+ }
+}
