74ed4330a844d9c7dfdde48250059418af256b1c
Becka Henry <36617288+beckahenryl@users.noreply.github.com>
2018-11-10 18:50:07 -0800

    Add files via upload

---
+++ CompareIgnore.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class CompareIgnore
+{
+ public static void main(String[] args)
+ {
+   final double TAX_RATE = 0.0775;
+   
+   System.out.printf( "Sales tax rate: %.2f%%", TAX_RATE * 100 );
+}
+}
+
